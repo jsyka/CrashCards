89ca690be1c34b4edf4025ed24a3e67108b8d6e3
Jessica Jiao <j7jiao@uwaterloo.ca>
2024-08-17 15:50:30 -0400

    Modified styling for sidebar and decklist

--- src/components/DeckPage/DeckPage.jsx
+++ src/components/DeckPage/DeckPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./DeckPage.css";
+import Cards from "../HeroPage/Card";
 
 const DeckPage = () => {
   return (
     <div className="deck-page">
       <div className="deck-page-container">
         <h1>Decklist</h1>
+        <Cards/>
       </div>
     </div>
   );
