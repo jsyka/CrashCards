b4bd5ac86d05b3b9d396764811edb26f0c7e27b1
BambooShi <110565810+BambooShi@users.noreply.github.com>
2024-08-17 02:53:12 -0400

    Added two new tables (front of card and back of card)

--- crashcards/crash_cards/admin.py
+++ crashcards/crash_cards/admin.py
@@ -3,3 +3,5 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Card)
+admin.site.register(Card_Front)
+admin.site.register(Card_Back)
--- crashcards/crash_cards/serializer.py
+++ crashcards/crash_cards/serializer.py
@@ -5,3 +5,13 @@ class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
         fields = '__all__'
+        
+class CardFrontSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Card_Front
+        fields = '__all__'
+        
+class CardBackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Card_Back
+        fields = '__all__'
