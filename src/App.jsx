e6ca97b5702700f752a78cb20f58b789a40a1815
Angela Huang <93748376+candycane124@users.noreply.github.com>
2024-08-17 12:44:41 -0400

    Fixed merge issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, } from 'react'
+import axios from 'axios'
 import './App.css'
 import HeroPage from './components/HeroPage/Hero.jsx'
 import Cards from './components/CardPage/Card.jsx'
 
 function App() {
-
-  // for retrieving cards from the database
-  const [cards, setCard] = useState("");
-
-  useEffect(() => {
-    axios.get("/api/cards/")
-      .then((response) => {
-        console.log(response.data);  // Log the response data
-        if (response.data.length > 0) {
-          setCard(response.data);  // Chooses what field to display from db
-        } else {
-          setCard("No cards found");
-        }
-      })
-      .catch((error) => {
-        console.error("There was an error!", error);
-        setCard("Error fetching cards");
-      });
-  }, []);
 
   // for retrieving users from the database
   const [users, setUser] = useState("");
@@ -44,28 +26,28 @@ function App() {
   }, []);
 
   // for passing data to db
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   formState: { errors },
+  // } = useForm();
 
-  const onSubmit = (data) => {
-    axios
-      .post("/api/user/", data)
-      .then((response) => {
-        console.log("Form submitted successfully", response);
-      })
-      .catch((error) => {
-        console.error("There was an error!", error);
-      });
-};
+  // const onSubmit = (data) => {
+  //   axios
+  //     .post("/api/user/", data)
+  //     .then((response) => {
+  //       console.log("Form submitted successfully", response);
+  //     })
+  //     .catch((error) => {
+  //       console.error("There was an error!", error);
+  //     });
+  // };
 
   return (
     <>
       <HeroPage />
       <Cards />
-      <h1>Users</h1>
+      {/* <h1>Users</h1>
       <div className="cards">
         {users.length > 0 ? (
           users.map((user) => (
@@ -76,7 +58,7 @@ function App() {
         ) : (
           <p>No users available</p>
         )}
-      </div>
+      </div> */}
     </>
   )
 }
