268f02a2f2d5ef45b8de2a2f8c6fa7f0a99d4038
Angela Huang <93748376+candycane124@users.noreply.github.com>
2024-08-17 21:59:51 -0400

    Improved hero page css and edited gemini

--- src/components/HeroPage/Hero.jsx
+++ src/components/HeroPage/Hero.jsx
@@ -6,15 +6,15 @@ export default function HeroPage() {
     return (
         <div className="hero">
             <div className="hero-banner">
-                <h1 className="title">Welcome to <span className="accent">CrashCards</span></h1>
+                <h1 className="title">Welcome to <span className="accent">CrashCards</span>!</h1>
                 <div className="buttons">
-                    {/* <button id="learn-more">Learn More</button> */}
-                    <button id="sign-in">Sign In</button>
+                    <button id="log-in">Log In</button>
+                    <button id="sign-in">Sign Up</button>
                 </div>
             </div>
             <div className="description">
                 <h2>What is CrashCards?</h2>
-                <div>the best flashcard site in the world!!!</div>
+                <div>the best flashcard site ever! no premium plans, hidden fees, or paywalls!</div>
             </div>
         </div>
     )
