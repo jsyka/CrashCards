67f1f6501bd4811b41cb7ded493541bbd3cb0525
Jessica Jiao <j7jiao@uwaterloo.ca>
2024-08-18 03:20:15 -0400

    Linked buttons on hero page to singup /login

--- src/components/HeroPage/Hero.jsx
+++ src/components/HeroPage/Hero.jsx
@@ -1,23 +1,30 @@
-import './Hero.css'
-import Cards from '../DeckElements/Card.jsx'
-import { Link } from 'react-router-dom'
+import "./Hero.css";
+import Cards from "../DeckElements/Card.jsx";
+import { Link } from "react-router-dom";
 
 export default function HeroPage() {
-
-    return (
-        <div className="hero">
-            <div className="hero-banner">
-                <h1 className="title">Welcome to <span className="accent">CrashCards</span>!</h1>
-                <div className="buttons">
-                
-                    <button id="log-in" >Log In</button>
-                    <button id="sign-in">Sign Up</button>
-                </div>
-            </div>
-            <div className="description">
-                <h2>What is CrashCards?</h2>
-                <p>the best flashcard site ever! no premium plans, hidden fees, or paywalls!</p>
-            </div>
+  return (
+    <div className="hero">
+      <div className="hero-banner">
+        <h1 className="title">
+          Welcome to <span className="accent">CrashCards</span>!
+        </h1>
+        <div className="buttons">
+          <Link to="/login">
+            <button id="log-in">Log In</button>
+          </Link>
+          <Link to="/sign-up">
+            <button id="sign-in">Sign Up</button>
+          </Link>
         </div>
-    )
+      </div>
+      <div className="description">
+        <h2>What is CrashCards?</h2>
+        <p>
+          the best flashcard site ever! no premium plans, hidden fees, or
+          paywalls!
+        </p>
+      </div>
+    </div>
+  );
 }
